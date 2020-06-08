a8ce9f5c73da6011c2ff783b8fff689f64b77972
stardustrain <kthanterran@gmail.com>
2020-06-09 00:09:23 +0900

    Fix .GetBiz function

--- utils/constants.R
+++ utils/constants.R
@@ -3,12 +3,12 @@ library(rvest)
 library(stringr)
 
 .GetBizDay <- function() {
-  url <- 'https://finance.naver.com/sise/sise_deposit.nhn'
+  url <- 'https://finance.naver.com'
   data <- GET(url)
 
   purrr::compose(
     function (x) read_html(x, encoding = 'EUC-KR'),
-    function (x) html_nodes(x, xpath = '//*[@id="type_1"]/div/ul[2]/li/span'),
+    function (x) html_node(x, '#time'),
     html_text,
     function (x) str_match(x, ('[0-9]+.[0-9]+.[0-9]+')),
     function (x) str_replace_all(x, '\\.', ''),
