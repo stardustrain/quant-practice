b67814c7771da5f25788a08192c2b395ed419cb6
stardustrain <kthanterran@gmail.com>
2020-06-08 23:05:31 +0900

    Remove commented TODAY variable

--- constants.R
+++ constants.R
@@ -11,12 +11,11 @@ library(stringr)
     function (x) html_nodes(x, xpath = '//*[@id="type_1"]/div/ul[2]/li/span'),
     html_text,
     function (x) str_match(x, ('[0-9]+.[0-9]+.[0-9]+')),
-    function (x) str_replace_all(x, '\\.', '')
-    , .dir='forward'
+    function (x) str_replace_all(x, '\\.', ''),
+    .dir='forward'
   )(data)
 }
 
-# TODAY = format(Sys.Date(), format='%Y%m%d')
 TODAY = .GetBizDay()
 GENERATE_OTP_URL = 'http://marketdata.krx.co.kr/contents/COM/GenerateOTP.jspx'
 DOWNLOAD_URL = 'http://file.krx.co.kr/download.jspx'
